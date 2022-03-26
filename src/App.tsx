0635dfde76491aa4fb9ac0573ab9e1cc77518c62
Filipe Dias <filipediascontato@gmail.com>
2022-03-26 15:45:30 -0300

    Fixes GlobalStyles import

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Page from "./components/Page";
-import { GlobalStyles } from "./GlobalStyles";
+import { GlobalStyles } from "./styles/GlobalStyles";
 
 function App() {
   return (
