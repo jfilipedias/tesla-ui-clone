971a9b23615c7c5374277dbde8e4c940560c71b9
Filipe Dias <filipediascontato@gmail.com>
2022-03-26 15:34:28 -0300

    Mocks the home page

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -4,20 +4,60 @@ import DefaultOverlayComponent from "../DefaultOverlayComponent";
 import { ModelsWrapper, ModelSection } from "../Models";
 import { Container } from "./styles";
 
+interface Model {
+  name: string;
+  description: string;
+}
+
+const models: Model[] = [
+  {
+    name: "Model One",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Two",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Three",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Four",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Five",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Six",
+    description: "Order online for delivery",
+  },
+  {
+    name: "Model Seven",
+    description: "Order online for delivery",
+  },
+];
+
 const Page: React.FC = () => {
   return (
     <Container>
       <ModelsWrapper>
         <div>
-          <ModelSection
-            modelName="Model One"
-            overlayNode={
-              <DefaultOverlayComponent
-                label="Model One"
-                description="Order online for delivery"
-              />
-            }
-          />
+          {models.map((model) => (
+            <ModelSection
+              key={model.name}
+              className="colored"
+              modelName={model.name}
+              overlayNode={
+                <DefaultOverlayComponent
+                  label={model.name}
+                  description={model.description}
+                />
+              }
+            />
+          ))}
         </div>
       </ModelsWrapper>
     </Container>
