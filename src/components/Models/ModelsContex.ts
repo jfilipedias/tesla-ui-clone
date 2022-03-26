ac0998dccc9247e89a4d381f80fe732ad486602d
Filipe Dias <filipediascontato@gmail.com>
2022-03-26 16:15:57 -0300

    Creates the ModelsContext

---
+++ src/components/Models/ModelsContex.ts
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface CarModel {
+  name: string;
+  overlayNode: React.ReactNode;
+  sectionRef: React.RefObject<HTMLElement>;
+}
+
+interface ModelsContext {
+  wrapperRef: React.RefObject<HTMLElement>;
+  registeredModels: CarModel[];
+  registerModel: (model: CarModel) => void;
+  unregisterModel: (modelName: string) => void;
+  getModelByName: (modelName: string) => CarModel;
+}
+
+export default React.createContext<ModelsContext>({} as ModelsContext);
