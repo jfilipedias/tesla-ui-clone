057edb104bb61c2768037be60a806fdd61d3eb3a
Filipe Dias <filipediascontato@gmail.com>
2022-03-26 15:16:03 -0300

    Creates GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { GlobalStyles } from "./GlobalStyles";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
